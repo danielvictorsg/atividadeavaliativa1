c659bbf24f0c1ec5eb64cd4a91a815612f2bff45
danielvictorsg <danielvictor.dszg@gmail.com>
2023-09-28 23:58:11 -0300

    commit final

--- questao3.c
+++ questao3.c
@@ -1,28 +1,22 @@
 #include <stdio.h>
 
 int main() {
-    int senha_cadastrada;
+    int senha_cadastrada = 0;
     int senha_digitada;
-    int tentativas = 0;
     int max_tentativas = 3;
 
     scanf("%d", &senha_cadastrada);
 
-    while (tentativas < max_tentativas) {
+    for (int tentativas = 0; tentativas < max_tentativas; tentativas++) {
         scanf("%d", &senha_digitada);
 
         if (senha_digitada == senha_cadastrada) {
             printf("Senha valida!\n");
-            break;
+            return 0; // Saída bem-sucedida
         } else {
             printf("Senha invalida!\n");
-            tentativas++;
         }
     }
 
-    if (tentativas == max_tentativas) {
-        printf("Acesso negado.\n");
-    }
-
-    return 0;
+    return 0; // Saída bem-sucedida
 }
--- questao4.c
+++ questao4.c
@@ -1,7 +1,30 @@
 #include <stdio.h>
-#include <stdlib.h>
 
-int main(){
-    
+int main() {
+    char nivel;
+    double salario, aumento, novo_salario;
+
+    scanf(" %c", &nivel);
+    scanf("%lf", &salario);
+
+    switch (nivel) {
+        case 'a':
+            aumento = salario * 0.05;
+            break;
+        case 'b':
+            aumento = salario * 0.07;
+            break;
+        case 'c':
+            aumento = salario * 0.08;
+            break;
+        default:
+            printf("Nível de experiência inválido.\n");
+            return 1;
+    }
+
+    novo_salario = salario + aumento;
+
+    printf("O novo salário do funcionário é: %.2lf\n", novo_salario);
+
     return 0;
 }
