742187b6d112686cafa5974c9323d67e582bb6c5
danielvictorsg <danielvictor.dszg@gmail.com>
2023-12-06 16:03:41 -0300

    commit 2

--- ProjetoFinal/projetofinal.c
+++ ProjetoFinal/projetofinal.c
@@ -18,6 +18,15 @@ void inserirHospede(char quartos[], int numeroQuarto) {
         printf("O quarto %d está ocupado. Escolha outro quarto.\n", numeroQuarto);
     }
 }
+
+void listarHospedes(Hospede hospedes[]) {
+    printf("Lista de Hospedes:\n");
+    for (int i = 0; i < NUM_QUARTOS; i++) {
+        if (hospedes[i].quarto != 0) {
+            printf("Quarto %d: %s\n", hospedes[i].quarto, hospedes[i].nome);
+        }
+    }
+}
 // Função principal
 int main() {
     char estadoQuartos[NUM_QUARTOS]; // Array para armazenar o estado dos quartos ('V' para vazio, 'O' para ocupado)
