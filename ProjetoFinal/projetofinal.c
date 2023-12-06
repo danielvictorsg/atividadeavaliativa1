b5a4dfcbef066f0d5c983e868eb425de63aeb00b
danielvictorsg <danielvictor.dszg@gmail.com>
2023-12-06 15:58:09 -0300

    commit 1

---
+++ ProjetoFinal/projetofinal.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <string.h>
+
+#define NUM_QUARTOS 10
+#define TAM_NOME 50
+int i = 0;
+typedef struct {
+    char nome[TAM_NOME];
+    int quarto;
+} Hospede;
+ 
+// Função para inserir um novo hóspede em um quarto vazio
+void inserirHospede(char quartos[], int numeroQuarto) {
+    if (quartos[numeroQuarto - 1] == 'V') {
+        quartos[numeroQuarto - 1] = 'O'; // 'O' representa ocupado
+        printf("Hóspede inserido no quarto %d com sucesso.\n", numeroQuarto);
+    } else {
+        printf("O quarto %d está ocupado. Escolha outro quarto.\n", numeroQuarto);
+    }
+}
+// Função principal
+int main() {
+    char estadoQuartos[NUM_QUARTOS]; // Array para armazenar o estado dos quartos ('V' para vazio, 'O' para ocupado)
+    int numeroHospede;
+ 
+    // Inicializar todos os quartos como vazios
+    for (i; i < NUM_QUARTOS; i++) {
+        estadoQuartos[i] = 'V';
+    }
+ 
+    // Solicitar número do hóspede ao usuário
+    printf("Digite o número do hóspede: ");
+    scanf("%d", &numeroHospede);
+ 
+
+    return 0;
+}
